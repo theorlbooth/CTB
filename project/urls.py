b67ce903ebffbcbb805aafcbfc72350e000418ce
Theo Booth <theorlbooth@googlemail.com>
2021-03-22 15:49:47 +0000

    Sales Endpoints

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/beers/', include('beers.urls'))
+    path('api/beers/', include('beers.urls')),
+    path('api/sales/', include('sales.urls'))
 ]
