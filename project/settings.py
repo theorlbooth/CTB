2d15339ac92c216c4bc12b2e6756ac60e62e5013
Theo Booth <theorlbooth@googlemail.com>
2021-03-23 16:29:11 +0000

    Login + Register

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'beers',
-    'sales'
+    'sales',
+    'jwt_auth'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'jwt_auth.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'jwt_auth.authentication.JWTAuthentication'
+    ],
+}
--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/beers/', include('beers.urls')),
-    path('api/sales/', include('sales.urls'))
+    path('api/sales/', include('sales.urls')),
+    path('api/auth/', include('jwt_auth.urls'))
 ]
